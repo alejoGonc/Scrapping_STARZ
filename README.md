05afc174f2e5dcb2c69ad60d2f6c45a81d5eba07
alejoGonc <65875123+alejoGonc@users.noreply.github.com>
2024-02-22 21:29:50 -0300

    scrap seasons

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Scrapping_STARZ
 
 ## Enunciado:
-- Obtener todas las películas y series.
-- Obtener la metadata de cada contenido: título, año, sinopsis, link, duración.
+- Obtener todas las películas y series. -- LISTO
+- Obtener la metadata de cada contenido: título, año, sinopsis, link, duración. -- LISTO
 - Guardar la información obtenida en una base de datos (cualquiera elegida) y además en archivo .json o .csv automáticamente.
 - PLUS: Obtener episodios de cada serie.
 - PLUS: Metadata de los episodios
--- scrape.py
+++ scrape.py
@@ -9,8 +9,6 @@ import time
 from selenium.common.exceptions import NoSuchElementException
 
 
-##FALTAN LINKS
-
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 driver.maximize_window()
@@ -26,47 +24,49 @@ movies_links = driver.find_elements(By.CLASS_NAME, "StandardSlide_img-container_
 movies = []
 
 
-for movie in movies_links:
-    href = movie.get_attribute("href")
-    if href not in movies:
-        movies.append(href)
+# for movie in movies_links:
+#     href = movie.get_attribute("href")
+#     if href not in movies:
+#         movies.append(href)
 
-movies_dataset = []
+# movies_dataset = []
 
-for link in movies[:2]:
-    driver.get(link)
-    time.sleep(5)
+# for link in movies[:2]:
+#     driver.get(link)
+#     time.sleep(5)
 
-    try:
+#     try:
 
-        movie_name = driver.find_element(By.XPATH, "//div/h1").text
-        movie_rating = driver.find_element(By.XPATH, "//div/span[1]").text
-        movie_duration = driver.find_element(By.XPATH, "//div/span[2]").text
-        movie_category = driver.find_element(By.XPATH, "//div/span[3]").text
-        movie_year = driver.find_element(By.XPATH, "//div/span[4]").text
-        movie_cast = driver.find_element(By.XPATH, "//p[@class='important-cast text-center text-xl-left pb-0 m-0 on-surface-1-variant-1']").text[9:]
+#         movie_name = driver.find_element(By.XPATH, "//div/h1").text
+#         movie_rating = driver.find_element(By.XPATH, "//div/span[1]").text
+#         movie_duration = driver.find_element(By.XPATH, "//div/span[2]").text
+#         movie_category = driver.find_element(By.XPATH, "//div/span[3]").text
+#         movie_year = driver.find_element(By.XPATH, "//div/span[4]").text
+#         movie_cast = driver.find_element(By.XPATH, "//p[@class='important-cast text-center text-xl-left pb-0 m-0 on-surface-1-variant-1']").text[9:]
 
-        try:
-            synopsis_button = driver.find_element(By.XPATH, "//p/button")
-            synopsis_button.click()
-            movie_synopsis = driver.find_element(By.XPATH, "//div/p[@class='on-surface-1 text-center mt-2 mb-1 mx-2']").text
-        except:
-            pass
+#         try:
+#             synopsis_button = driver.find_element(By.XPATH, "//p/button")
+#             synopsis_button.click()
+#             time.sleep(2)
+#             movie_synopsis = driver.find_element(By.XPATH, "//div/p[@class='on-surface-1 text-center mt-2 mb-1 mx-2']").text
+#         except:
+#             pass
 
-        movies_dataset.append(
-            {"Name":movie_name,
-             "Rating":movie_rating,
-             "Duration":movie_duration,
-             "Category":movie_category,
-             "Year":movie_year,
-             "Cast":movie_cast,
-             "Synopsis":movie_synopsis
-            }
-        )
+#         movies_dataset.append(
+#             {"Name":movie_name,
+#              "Rating":movie_rating,
+#              "Duration":movie_duration,
+#              "Category":movie_category,
+#              "Year":movie_year,
+#              "Cast":movie_cast,
+#              "Synopsis":movie_synopsis,
+#              "Link":link
+#             }
+#         )
 
-    except NoSuchElementException:
+#     except NoSuchElementException:
 
-        print("Element not found in:", link)
+#         print("Element not found in:", link)
 
 
 series_url = 'https://www.starz.com/us/en/series'
@@ -85,6 +85,7 @@ for series in series_links:
         series_list.append(href)
 
 series_dataset = []
+season_dataset = []
 
 for series_link in series_list[:4]:
     driver.get(series_link)
@@ -102,9 +103,10 @@ for series_link in series_list[:4]:
         try:
             synopsis_button = driver.find_element(By.XPATH, "//p/button")
             synopsis_button.click()
+            time.sleep(2)
             series_synopsis = driver.find_element(By.XPATH, "//div/p[@class='on-surface-1 text-center mt-2 mb-1 mx-2']").text
         except:
-            pass
+            series_synopsis = None
 
         series_dataset.append(
             {"Name":series_name,
@@ -113,13 +115,39 @@ for series_link in series_list[:4]:
              "Category":series_category,
              "Duration":series_duration,
              "Cast":series_cast,
-             "Synopsis":series_synopsis
+             "Synopsis":series_synopsis,
+             "Link":series_link
             }
         )
 
-    except NoSuchElementException:
+        series_url = driver.current_url
 
-        print("Element not found in:", link)
+        season_links = driver.find_elements(By.XPATH, "//div[@class='SeasonCard_slide__hTAA_ col-xs-12 col-sm-6 col-md-4 col-xl-4']/div[@class='SeasonCard_image-container__9YQZy']/a[@class='SeasonCard_hover-content__Qvwds']")
+        season_number = [season.get_attribute("href") for season in season_links]
+        print(season_number)
+
+        for season in season_number:
+            driver.get(season)
+            time.sleep(3)
+
+            season_name = driver.find_element(By.XPATH, "//button/span").text
+            season_episodes = driver.find_element(By.XPATH, "//div/h2").text
+            season_synopsis = driver.find_element(By.XPATH, "//p/span").text
+
+            season_dataset.append({
+                "Series": series_name,
+                "Season": season_name,
+                "Episodes": season_episodes,
+                "Synopsis": season_synopsis,
+                "Link": season
+            })
+
+    except NoSuchElementException:
+         print("Element not found in:", series_link)
+
+# for season_data in season_dataset:
+#     print(season_data)
+
 
 
 driver.quit()
